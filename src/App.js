648f72ee90e5699fa4dc2fd192b1459c444888fd
Denis Kiselev <k1selevde@yandex.ru>
2020-03-24 12:37:46 +0500

    init project struct

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h5>Fuck twelve</h5>
+    </div>
+  );
+}
+
+export default App;
